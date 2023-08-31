b679f0c061179bd9cc7fc1242b9ee20ea84b568b
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2023-08-31 22:21:32 +0700

    changeview on mobile. Changing space between each section.

--- src/assets/JS/index.js
+++ src/assets/JS/index.js
@@ -1,26 +1,31 @@
-const scrollPercent = 80;
+const scrollOffset = 100;
 const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
 
-const elementInView = (el, percentageScroll = 100) => {
+const elementInView = (el, offSet = 0, isHigher = true) => {
 	const elementTop = el.getBoundingClientRect().top;
+	const AdditionalValue = isHigher ? 1 : -1;
 	return (
 		elementTop <=
-		(window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100)
+		(window.innerHeight || document.documentElement.clientHeight) + offSet * AdditionalValue
 	);
 };
 
-const elementOutofView = (el) => {
+const elementOutofView = (el, offSet = 0, isHigher = true) => {
 	const elementTop = el.getBoundingClientRect().top;
-
-	return elementTop > (window.innerHeight || document.documentElement.clientHeight);
+	const AdditionalValue = isHigher ? 1 : -1;
+	return (
+		elementTop >
+		(window.innerHeight || document.documentElement.clientHeight) + offSet * AdditionalValue
+	);
 };
 
 const scrollElements = document.querySelectorAll('.scroll-animation');
 
 const scrollHandler = () => {
 	scrollElements.forEach((e) => {
-		if (elementInView(e, scrollPercent)) e.classList.add('scroll-animation-scrolled');
-		else if (elementOutofView(e)) e.classList.remove('scroll-animation-scrolled');
+		if (elementInView(e, scrollOffset, false)) e.classList.add('scroll-animation-scrolled');
+		else if (elementOutofView(e, scrollOffset, false))
+			e.classList.remove('scroll-animation-scrolled');
 	});
 };
 
