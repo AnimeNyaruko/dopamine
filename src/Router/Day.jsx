b749569712828f449cd2f0a291bc62d4a29cbc56
tqs <126000317+AnimeNyaruko@users.noreply.github.com>
2023-09-13 15:17:52 +0700

    Update Day.jsx

--- src/Router/Day.jsx
+++ src/Router/Day.jsx
@@ -19,11 +19,11 @@ export default function Day() {
 		const stage = param.changID;
 		switch (stage):
 			case '1':
-				if (dayNumber<= 10 &&dayNumber >0) return true;
+				if (dayNumber<= 10 && dayNumber >0) return true;
 			case '2':
 				if (dayNumber >10 && dayNumber <=20)return true;
 			case '3':
-				if(dayNumber > 20 %% dayNumber <=30) return true;
+				if(dayNumber > 20 && dayNumber <=30) return true;
 
 		return false;
 	};
