2505aafd4328ccc35287b4d340eb1562f1ec5693
AnimeNyaruko <robloxandminecraft1603@gmail.com>
2023-09-14 21:48:31 +0700

    change a little bit to style download page

--- src/Router/DownloadPDF.jsx
+++ src/Router/DownloadPDF.jsx
@@ -11,7 +11,7 @@ export default function DownloadPDF() {
 				e.target.click();
 				navigate('/');
 			}}
-			className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold cursor-pointer"
+			className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-2xl text-center font-bold cursor-pointer"
 			ref={ref}
 			href="/PDF/DOPAMINE RESET SHCE.pdf"
 			download={`DOPAMINE RESET SHCE.pdf`}>
